5edf22a2c19c30ff39c9d8a4ec8f302e0eccb83a
KidSea <594643219@qq.com>
2018-06-03 16:13:04 +0800

    init project value

--- app/src/main/java/com/leslie/musicplayer/MusicPlayMainActivity.java
+++ app/src/main/java/com/leslie/musicplayer/MusicPlayMainActivity.java
@@ -1,13 +1,14 @@
 package com.leslie.musicplayer;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MusicPlayMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        DataBindingUtil.setContentView(this, R.layout.activity_music_play_main);
     }
 }
