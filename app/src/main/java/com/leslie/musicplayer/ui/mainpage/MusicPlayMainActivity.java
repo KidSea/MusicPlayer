332b15ede2b558b29762ab66ba501350616b43cf
KidSea <594643219@qq.com>
2019-02-17 16:02:57 +0800

    add detail menu

--- app/src/main/java/com/leslie/musicplayer/ui/mainpage/MusicPlayMainActivity.java
+++ app/src/main/java/com/leslie/musicplayer/ui/mainpage/MusicPlayMainActivity.java
@@ -1,32 +1,37 @@
 package com.leslie.musicplayer.ui.mainpage;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.transition.Fade;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.leslie.musicplayer.R;
 import com.leslie.musicplayer.base.BaseMusicActivity;
 
 public class MusicPlayMainActivity extends BaseMusicActivity implements
-        NavigationView.OnNavigationItemSelectedListener {
+        NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
+    private FrameLayout mFlTitleMenu;
+    private ImageView mIvTitleOne;
+    private ImageView mIvTitleTwo;
+    private ImageView mIvTitleThree;
+    private ViewPager mVpContent;
 
-    private ActionBarDrawerToggle mDrawerToggle;
     private long exitTime;
 
     @Override
@@ -56,6 +61,32 @@ public class MusicPlayMainActivity extends BaseMusicActivity implements
     }
 
     @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_title_menu:
+                // 开启菜单
+                toggleDrawer();
+                break;
+            case R.id.iv_title_two:
+                // 不然cpu会有损耗
+//                if (mVpContent.getCurrentItem() != 1) {
+                    setCurrentItem(1);
+//                }
+                break;
+            case R.id.iv_title_one:
+//                if (mVpContent.getCurrentItem() != 0) {
+                    setCurrentItem(0);
+//                }
+                break;
+            case R.id.iv_title_three:
+//                if (mVpContent.getCurrentItem() != 2) {
+                    setCurrentItem(2);
+//                }
+                break;
+        }
+    }
+
+    @Override
     protected int requestLayoutId() {
         return R.layout.music_play_main;
     }
@@ -67,12 +98,6 @@ public class MusicPlayMainActivity extends BaseMusicActivity implements
     }
 
     @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
-    }
-
-    @Override
     protected void initData() {
         super.initData();
         mNavigationView.setNavigationItemSelectedListener(this);
@@ -81,27 +106,63 @@ public class MusicPlayMainActivity extends BaseMusicActivity implements
     @Override
     protected void initView() {
         super.initView();
-        mToolbar = (Toolbar)findViewById(R.id.app_toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.app_toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mFlTitleMenu = (FrameLayout) findViewById(R.id.ll_title_menu);
+        mIvTitleOne = (ImageView) findViewById(R.id.iv_title_one);
+        mIvTitleTwo = (ImageView) findViewById(R.id.iv_title_two);
+        mIvTitleThree = (ImageView) findViewById(R.id.iv_title_three);
+        mVpContent = (ViewPager) findViewById(R.id.vp_content);
+        mIvTitleOne.setSelected(true);
+
+        initListener();
     }
 
     @Override
     protected void initActionBar() {
-        mToolbar.setTitle(R.string.app_name);
-        mToolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(mToolbar);
-        mToolbar.setNavigationIcon(R.drawable.img_slide_menu);
-        //设置菜单按钮动画
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
-                R.string.drawer_open, R.string.drawer_close);
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleDrawer();
-            }
-        });
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            //去除默认Title显示
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    private void initListener() {
+        mFlTitleMenu.setOnClickListener(this);
+        mIvTitleOne.setOnClickListener(this);
+        mIvTitleTwo.setOnClickListener(this);
+        mIvTitleThree.setOnClickListener(this);
+    }
+
+    /**
+     * 切换页面
+     *
+     * @param position 分类角标
+     */
+    private void setCurrentItem(int position) {
+        boolean isOne = false;
+        boolean isTwo = false;
+        boolean isThree = false;
+        switch (position) {
+            case 0:
+                isOne = true;
+                break;
+            case 1:
+                isTwo = true;
+                break;
+            case 2:
+                isThree = true;
+                break;
+            default:
+                isTwo = true;
+                break;
+        }
+//        mVpContent.setCurrentItem(position);
+        mIvTitleOne.setSelected(isOne);
+        mIvTitleTwo.setSelected(isTwo);
+        mIvTitleThree.setSelected(isThree);
     }
 
     private void toggleDrawer() {
@@ -111,5 +172,4 @@ public class MusicPlayMainActivity extends BaseMusicActivity implements
             mDrawerLayout.openDrawer(GravityCompat.START);
         }
     }
-
 }
