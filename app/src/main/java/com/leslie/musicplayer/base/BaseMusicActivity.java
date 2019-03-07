1e0033e21fd30697936302acd8cb11a20b3e08e3
KidSea <594643219@qq.com>
2019-03-07 20:46:00 +0800

    add View to MiniPlay

--- app/src/main/java/com/leslie/musicplayer/base/BaseMusicActivity.java
+++ app/src/main/java/com/leslie/musicplayer/base/BaseMusicActivity.java
@@ -1,13 +1,16 @@
 package com.leslie.musicplayer.base;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
 import com.leslie.musicplayer.R;
 import com.leslie.musicplayer.ui.mainpage.fragment.QuickControlsFragment;
+import com.leslie.musicplayer.utils.CommonThreadPool;
 
 public class BaseMusicActivity extends BaseActivity {
 
-    private QuickControlsFragment fragment; //底部播放控制栏
+    private QuickControlsFragment mControlsFragment; //底部播放控制栏
 
     protected void initView() {
         showQuickControl(true);
@@ -16,18 +19,29 @@ public class BaseMusicActivity extends BaseActivity {
     /**
      * @param show 显示或关闭底部播放控制栏
      */
-    private void showQuickControl(boolean show) {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        if (show) {
-            if (fragment == null) {
-                fragment = QuickControlsFragment.newInstance();
-                ft.add(R.id.bottom_container, fragment).commitAllowingStateLoss();
-            } else {
-                ft.show(fragment).commitAllowingStateLoss();
+    private void showQuickControl(final boolean show) {
+        CommonThreadPool.COMMON_THREAD_POOL.execute(new Runnable() {
+            @Override
+            public void run() {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                if (show) {
+                    if (mControlsFragment == null) {
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        Fragment fragment = fragmentManager.findFragmentById(R.id.bottom_container);
+                        if (fragment instanceof QuickControlsFragment) {
+                            mControlsFragment = (QuickControlsFragment) fragment;
+                        } else {
+                            mControlsFragment = new QuickControlsFragment();
+                        }
+                        ft.add(R.id.bottom_container, mControlsFragment).commitAllowingStateLoss();
+                    } else {
+                        ft.show(mControlsFragment).commitAllowingStateLoss();
+                    }
+                } else {
+                    if (mControlsFragment != null)
+                        ft.hide(mControlsFragment).commitAllowingStateLoss();
+                }
             }
-        } else {
-            if (fragment != null)
-                ft.hide(fragment).commitAllowingStateLoss();
-        }
+        });
     }
 }
--- app/src/main/java/com/leslie/musicplayer/ui/mainpage/fragment/QuickControlsFragment.java
+++ app/src/main/java/com/leslie/musicplayer/ui/mainpage/fragment/QuickControlsFragment.java
@@ -1,16 +1,50 @@
 package com.leslie.musicplayer.ui.mainpage.fragment;
 
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.leslie.musicplayer.R;
 import com.leslie.musicplayer.base.BaseFragment;
+import com.leslie.musicplayer.utils.ToastUtil;
+import com.leslie.musicplayer.utils.ViewUtils;
 
 /**
  * Created by yuxuehai on 19-2-26.
  */
-public class QuickControlsFragment extends BaseFragment {
-    private static QuickControlsFragment fragment;
+public class QuickControlsFragment extends BaseFragment implements View.OnClickListener {
+    private FrameLayout mMiniContainer;
+    private ImageView mAlbumCover;
+    private ImageView mMusicNext;
+    private ImageView mMusicPlay;
+    private ImageView mAddFavorite;
+    private TextView mMusicSlogan;
+    private TextView mTitle;
+    private TextView mArtist;
 
-    public static QuickControlsFragment newInstance() {
-        return new QuickControlsFragment();
+    @Override
+    protected void initView(View view) {
+        super.initView(view);
+        mMiniContainer = ViewUtils.findView(view, R.id.fl_mini_container);
+        mAlbumCover = ViewUtils.findView(view, R.id.iv_album_cover);
+        mMusicNext = ViewUtils.findView(view, R.id.iv_next);
+        mMusicPlay = ViewUtils.findView(view, R.id.iv_play);
+        mAddFavorite = ViewUtils.findView(view, R.id.iv_add_to_favorite);
+        mMusicSlogan = ViewUtils.findView(view, R.id.tv_music_slogan);
+        mTitle = ViewUtils.findView(view, R.id.tv_title_text);
+        mArtist = ViewUtils.findView(view, R.id.tv_artist_text);
+
+        mMiniContainer.setOnClickListener(this);
+        mAlbumCover.setOnClickListener(this);
+        mMusicNext.setOnClickListener(this);
+        mMusicPlay.setOnClickListener(this);
+        mAddFavorite.setOnClickListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
@@ -18,5 +52,19 @@ public class QuickControlsFragment extends BaseFragment {
         return R.layout.mini_music_fragment;
     }
 
-
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        ToastUtil.showToast("show id: " + id);
+        switch (id) {
+            case R.id.iv_album_cover:
+                break;
+            case R.id.iv_next:
+                break;
+            case R.id.iv_play:
+                break;
+            case R.id.iv_add_to_favorite:
+                break;
+        }
+    }
 }
