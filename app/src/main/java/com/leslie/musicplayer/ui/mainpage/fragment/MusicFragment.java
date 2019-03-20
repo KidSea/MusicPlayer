0e76a3cd33111ff49e3ba7b103b10ca6ba97bfeb
KidSea <594643219@qq.com>
2019-03-20 19:59:28 +0800

    develop local music day2

--- app/src/main/java/com/leslie/musicplayer/ui/mainpage/fragment/MusicFragment.java
+++ app/src/main/java/com/leslie/musicplayer/ui/mainpage/fragment/MusicFragment.java
@@ -1,14 +1,19 @@
 package com.leslie.musicplayer.ui.mainpage.fragment;
 
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.leslie.musicplayer.R;
 import com.leslie.musicplayer.base.BaseFragment;
+import com.leslie.musicplayer.utils.ViewUtils;
 
 /**
  * Created by yuxuehai on 19-02-20.
  */
 public class MusicFragment extends BaseFragment {
+
+    private RecyclerView mMuiscListRecyclerView;
+
     @Override
     protected int requestLayoutId() {
         return R.layout.music_main_fragment;
@@ -17,5 +22,11 @@ public class MusicFragment extends BaseFragment {
     @Override
     protected void initView(View view) {
         super.initView(view);
+        mMuiscListRecyclerView = ViewUtils.findView(view, R.id.music_list_recyclerview);
+    }
+
+    @Override
+    protected void loadData() {
+        super.loadData();
     }
 }
