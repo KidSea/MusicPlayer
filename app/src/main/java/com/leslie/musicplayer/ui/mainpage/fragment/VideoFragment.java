760ca66a1546160113df602a5b1edda24b0bb98a
KidSea <594643219@qq.com>
2019-03-21 21:04:11 +0800

    add basemodelfragment

--- app/src/main/java/com/leslie/musicplayer/ui/mainpage/fragment/VideoFragment.java
+++ app/src/main/java/com/leslie/musicplayer/ui/mainpage/fragment/VideoFragment.java
@@ -1,24 +1,21 @@
 package com.leslie.musicplayer.ui.mainpage.fragment;
 
-import android.view.View;
-import android.widget.TextView;
-
 import com.leslie.musicplayer.R;
-import com.leslie.musicplayer.base.BaseFragment;
+import com.leslie.musicplayer.base.BaseModelFragment;
+import com.leslie.musicplayer.databinding.MainFragmentBinding;
+import com.leslie.musicplayer.viewModel.NoViewModel;
 
 /**
  * Created by yuxuehai on 19-02-20.
  */
-public class VideoFragment extends BaseFragment {
+public class VideoFragment extends BaseModelFragment<NoViewModel, MainFragmentBinding> {
     @Override
     protected int requestLayoutId() {
         return R.layout.main_fragment;
     }
 
     @Override
-    protected void initView(View view) {
-        super.initView(view);
-        TextView ivContent = view.findViewById(R.id.iv_content);
-        ivContent.setText("Video");
+    protected void initView() {
+        mBindingView.ivContent.setText("Video");
     }
 }
