d1b0c465e77b50d7aa209c95a37d74618304ebf8
KidSea <594643219@qq.com>
2019-04-01 20:56:41 +0800

    develop local music day7

--- app/src/main/java/com/leslie/musicplayer/ui/localmusic/MusicFragment.java
+++ app/src/main/java/com/leslie/musicplayer/ui/localmusic/MusicFragment.java
@@ -2,6 +2,8 @@ package com.leslie.musicplayer.ui.localmusic;
 
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -12,6 +14,7 @@ import com.leslie.musicplayer.common.MyLinearLayoutManager;
 import com.leslie.musicplayer.databinding.MusicMainFragmentBinding;
 import com.leslie.musicplayer.ui.localmusic.adapter.MusicListAdapter;
 import com.leslie.musicplayer.viewModel.localmusic.MusicViewModel;
+import com.leslie.musicplayer.widget.CustomerScrollView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,7 @@ import java.util.List;
  * Created by yuxuehai on 19-02-20.
  */
 public class MusicFragment extends BaseModelFragment<MusicViewModel, MusicMainFragmentBinding> {
+    private static final String TAG = MusicFragment.class.getSimpleName();
 
     private MusicListAdapter adapter;
     private View mHeaderBinding;
@@ -42,6 +46,36 @@ public class MusicFragment extends BaseModelFragment<MusicViewModel, MusicMainFr
         mBindingView.musicListRecyclerview.setNestedScrollingEnabled(false);
         mBindingView.musicListRecyclerview.setHasFixedSize(false);
         mBindingView.musicListRecyclerview.setItemAnimator(new DefaultItemAnimator());
+        mBindingView.scrollLayout.setScrollChangedListener(new CustomerScrollView.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged(int x, int y, int oldxX, int oldY) {
+                Log.d(TAG, "onScrollChanged");
+                if (mBindingView.scrollLayout.isAtTop()) {
+                    mBindingView.musicListRecyclerview.setInterceptParent(false);
+                }else if (mBindingView.scrollLayout.isAtBottom()) {
+                    mBindingView.musicListRecyclerview.setInterceptParent(false);
+                } else {
+                    mBindingView.musicListRecyclerview.setInterceptParent(true);
+                }
+            }
+        });
+        mBindingView.musicListRecyclerview.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                //currentCanScrollDown记录当前列表的可滚动状态，false表示列表已经滑到底部边界,边界下边已经没内容了
+                boolean currentCanScrollDown = recyclerView.canScrollVertically(1);
+                //currentCanScrollUp记录当前列表的可滚动状态，false表示列表已经滑到顶部边界,边界上边已经没内容了
+                boolean currentCanScrollUp = recyclerView.canScrollVertically(-1);
+                mBindingView.musicListRecyclerview.setInterceptParent(!(currentCanScrollDown || currentCanScrollUp));
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/leslie/musicplayer/widget/CustomerScrollView.java
+++ app/src/main/java/com/leslie/musicplayer/widget/CustomerScrollView.java
@@ -11,102 +11,136 @@ import android.widget.ScrollView;
 
 /**
  * 可以拖动的ScrollView
- *
  */
 public class CustomerScrollView extends ScrollView {
 
-	private static final int size = 4;
-	private View inner;
-	private float y;
-	private Rect normal = new Rect();;
+    private static final int size = 4;
 
-	public CustomerScrollView(Context context) {
-		super(context);
-	}
+    private View inner;
 
-	public CustomerScrollView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    private OnScrollChangedListener mScrollChangedListener;
+    private float y;
+    private Rect normal = new Rect();
 
-	@Override
-	protected void onFinishInflate() {
-		if (getChildCount() > 0) {
-			inner = getChildAt(0);
-		}
-	}
+    public CustomerScrollView(Context context) {
+        super(context);
+    }
 
-	@SuppressLint("ClickableViewAccessibility")
-	@Override
-	public boolean onTouchEvent(MotionEvent ev) {
-		if (inner == null) {
-			return super.onTouchEvent(ev);
-		} else {
-			commOnTouchEvent(ev);
-		}
-		return super.onTouchEvent(ev);
-	}
+    public CustomerScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public void commOnTouchEvent(MotionEvent ev) {
-		int action = ev.getAction();
-		switch (action) {
-		case MotionEvent.ACTION_DOWN:
-			y = ev.getY();
-			break;
-		case MotionEvent.ACTION_UP:
-			if (isNeedAnimation()) {
-				// Log.v("mlguitar", "will up and animation");
-				animation();
-			}
-			break;
-		case MotionEvent.ACTION_MOVE:
-			final float preY = y;
-			float nowY = ev.getY();
-			/**
-			 * size=4 表示 拖动的距离为屏幕的高度的1/4
-			 */
-			int deltaY = (int) (preY - nowY) / size;
-			// 滚动
-			// scrollBy(0, deltaY);
+    @Override
+    protected void onFinishInflate() {
+        if (getChildCount() > 0) {
+            inner = getChildAt(0);
+        }
+    }
 
-			y = nowY;
-			if (isNeedMove()) {
-				if (normal.isEmpty()) {
-					normal.set(inner.getLeft(), inner.getTop(),
-							inner.getRight(), inner.getBottom());
-					return;
-				}
-				int yy = inner.getTop() - deltaY;
+    @Override
+    protected void onScrollChanged(int x, int y, int oldx, int oldy) {
+        super.onScrollChanged(x, y, oldx, oldy);
+        if (mScrollChangedListener != null) {
+            mScrollChangedListener.onScrollChanged(x, y, oldx, oldy);
+        }
+    }
 
-				// 移动布局
-				inner.layout(inner.getLeft(), yy, inner.getRight(),
-						inner.getBottom() - deltaY);
-			}
-			break;
-		default:
-			break;
-		}
-	}
+    public void setScrollChangedListener(OnScrollChangedListener scrollChangedListener) {
+        mScrollChangedListener = scrollChangedListener;
+    }
 
-	public void animation() {
-		TranslateAnimation ta = new TranslateAnimation(0, 0, inner.getTop(),
-				normal.top);
-		ta.setDuration(200);
-		inner.startAnimation(ta);
-		inner.layout(normal.left, normal.top, normal.right, normal.bottom);
-		normal.setEmpty();
-	}
+    /**
+     *
+     * @return
+     */
+    public boolean isAtTop(){
+        return getScrollY() <= 0;
+    }
+    /**
+     *
+     * @return
+     */
+    public boolean isAtBottom(){
+        return getScrollY() == getChildAt(getChildCount() - 1)
+                .getBottom() + getPaddingBottom() - getHeight();
+    }
 
-	public boolean isNeedAnimation() {
-		return !normal.isEmpty();
-	}
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (inner == null) {
+            return super.onTouchEvent(ev);
+        } else {
+            commOnTouchEvent(ev);
+        }
+        return super.onTouchEvent(ev);
+    }
 
-	public boolean isNeedMove() {
-		int offset = inner.getMeasuredHeight() - getHeight();
-		int scrollY = getScrollY();
-		if (scrollY == 0 || scrollY == offset) {
-			return true;
-		}
-		return false;
-	}
+    public void commOnTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                y = ev.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                if (isNeedAnimation()) {
+                    // Log.v("mlguitar", "will up and animation");
+                    animation();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final float preY = y;
+                float nowY = ev.getY();
+                /**
+                 * size=4 表示 拖动的距离为屏幕的高度的1/4
+                 */
+                int deltaY = (int) (preY - nowY) / size;
+                // 滚动
+                // scrollBy(0, deltaY);
+
+                y = nowY;
+                if (isNeedMove()) {
+                    if (normal.isEmpty()) {
+                        normal.set(inner.getLeft(), inner.getTop(),
+                                inner.getRight(), inner.getBottom());
+                        return;
+                    }
+                    int yy = inner.getTop() - deltaY;
+
+                    // 移动布局
+                    inner.layout(inner.getLeft(), yy, inner.getRight(),
+                            inner.getBottom() - deltaY);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void animation() {
+        TranslateAnimation ta = new TranslateAnimation(0, 0, inner.getTop(),
+                normal.top);
+        ta.setDuration(200);
+        inner.startAnimation(ta);
+        inner.layout(normal.left, normal.top, normal.right, normal.bottom);
+        normal.setEmpty();
+    }
+
+    public boolean isNeedAnimation() {
+        return !normal.isEmpty();
+    }
+
+    public boolean isNeedMove() {
+        int offset = inner.getMeasuredHeight() - getHeight();
+        int scrollY = getScrollY();
+        if (scrollY == 0 || scrollY == offset) {
+            return true;
+        }
+        return false;
+    }
+
+    public interface OnScrollChangedListener{
+        public void onScrollChanged(int x, int y, int oldxX, int oldY);
+    }
 
 }
